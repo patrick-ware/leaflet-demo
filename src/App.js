967b8b27652276f0d0323c404ea869b8474c5866
Patrick Ware <patrick.r.ware@gmail.com>
2021-03-16 21:21:31 -0700

    map container added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { Icon } from "leaflet";
 // Styles
 import './App.css';
 
 function App() {
   return (
-    <div>
-      Map
-    </div>
+    <MapContainer></MapContainer>
   );
 }
 
