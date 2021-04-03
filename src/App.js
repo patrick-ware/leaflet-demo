95f432c8d719eee76a0e538ec795f7cd3f991a88
Patrick Ware <patrick.r.ware@gmail.com>
2021-04-03 15:59:45 -0700

    demo marker added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
